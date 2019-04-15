1cc61066eecb2a4e1633018cc3fc570cd9961d20
Martin Connor <martinconnor@Martins-MacBook-Pro-2.local>
2019-04-15 14:40:12 -0400

    Implement a HoneyMakerBee, ForagerBee, and RetiredForagerBee pseudoclassical subclass.

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.honeyPot = 0;
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function () {
+  var newTreasure = 1
+  this.treasureChest.push(newTreasure);
+}
