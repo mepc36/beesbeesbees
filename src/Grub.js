69fe912a60e6c88b721f63848f354a1da71dbcb1
Martin Connor <martinconnor@Martins-MacBook-Pro-2.local>
2019-04-15 13:30:51 -0400

    Implement a 'Grub' subclass pseudoclassically.

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,16 @@
+// Pseudoclassical style:
+// invoke with "new"
+// doesn't explicitly define or return the objectl
+// uses the keyword "this";
+// declares functions outside, at the .prototype property;
+
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly'
+  debugger;
 };
 
+Grub.prototype.eat = function() {
+
+}
